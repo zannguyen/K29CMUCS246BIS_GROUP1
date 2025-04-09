8ece8ddaab907ee1790d6de391568d2155f14a88
HuyyPhan2005 <nhathuyphan21@gmail.com>
2025-04-09 17:59:55 +0700

    Thêm xử lý phép nhân vào Nhan.java

--- Lab_03/src/view/Nhan.java
+++ Lab_03/src/view/Nhan.java
@@ -1,5 +1,11 @@
 package view;
 
 public class Nhan {
-
+	try {
+        int a = getSo1();
+        int b = getSo2();
+        txtKetQua.setText(String.valueOf(a * b));
+    } catch (NumberFormatException ex) {
+        txtKetQua.setText("Lỗi định dạng số!");
+    }
 }
