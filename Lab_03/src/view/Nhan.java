4c33c60901e270b1110bbb1f72f500f9d9e43ab3
HuyyPhan2005 <nhathuyphan21@gmail.com>
2025-04-09 18:23:28 +0700

    Thêm xử lý phép nhân

--- Lab_03/src/view/Nhan.java
+++ Lab_03/src/view/Nhan.java
@@ -1,11 +1,7 @@
 package view;
 
 public class Nhan {
-	try {
-        int a = getSo1();
-        int b = getSo2();
-        txtKetQua.setText(String.valueOf(a * b));
-    } catch (NumberFormatException ex) {
-        txtKetQua.setText("Lỗi định dạng số!");
+	public static int tinh(int a, int b) {
+        return a * b;
     }
 }
